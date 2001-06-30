4762c0d442c31eae78a1b8a9b7d5a9080acdcc4c
Tanaka Akira <akr@fsij.org>
2001-06-30 06:21:02 +0000

    add templates and config.

---
+++ reviz/reviz-conf.rb
@@ -0,0 +1,8 @@
+class ReViz
+  CVSROOT_LIST = [
+    ['main', '/home/akr/.cvsroot'],
+    ['cvsroot2', '/home/akr/.cvsroot2'],
+  ]
+
+  HTML_TEMPLATE_DIR = '/home/akr/src/ruby/cvs/ruby-cvs/reviz-lib'
+end
